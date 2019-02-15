12d11a11597e081b625c1f3c2b5fecaec183a20b
Pedro Rafael <pedro.rafael.marinho@gmail.com>
2019-02-15 09:01:04 -0300

    update code fast.cpp

--- fast.cpp
+++ fast.cpp
@@ -1,3 +1,14 @@
+# Author: Pedro Rafael D. Marinho
+# Institution: Federal University of Paraíba, Brazil
+# Department of Statistics
+# Email: pedro@de.ufpb.br
+
+
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+
+
 #include <RcppArmadillo.h>
 #include <math.h>
 
@@ -5,15 +16,6 @@
 
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
 
 // [[Rcpp::export]]
 double S2_cpp(NumericVector y1, NumericVector y2) {
